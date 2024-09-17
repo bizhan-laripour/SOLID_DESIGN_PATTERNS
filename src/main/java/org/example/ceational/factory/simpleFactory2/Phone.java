1d4b7193530da456a09d09756e1b9444a2ab0995
104913641 <Bizh@n!8830183>
2024-09-17 11:49:56 +0330

    FIRST COMMIT

--- src/main/java/org/example/ceational/factory/simpleFactory2/Phone.java
+++ src/main/java/org/example/ceational/factory/simpleFactory2/Phone.java
@@ -3,6 +3,6 @@ package org.example.ceational.factory.simpleFactory2;
 public class Phone implements Product{
     @Override
     public void nameAndPrice() {
-        System.out.println("phone and price is 1000 dolors");
+        System.out.println("phone and price is 1000 dolor");
     }
 }
