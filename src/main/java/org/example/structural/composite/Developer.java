ffc65cdfb2ea826e75838bf5155f93082ac82f3b
104913641 <Bizh@n!8830183>
2024-09-24 12:50:30 +0330

    combination of bridge design and abstract factory

--- src/main/java/org/example/structural/composite/Developer.java
+++ src/main/java/org/example/structural/composite/Developer.java
@@ -1,53 +1,52 @@
-package org.example.structural.composition;
+package org.example.structural.composite;
 
-public class Manager implements Employee {
+public class Developer implements Employee{
     private final String name;
     private final String position;
     private final long employeeId;
-    private final int salary;
+    private int salary;
 
-    private Manager(String name, String position, long employeeId, int salary) {
+    private Developer(String name, String position, long employeeId , int salary) {
         this.name = name;
         this.position = position;
         this.employeeId = employeeId;
         this.salary = salary;
     }
-
     @Override
     public int showEmployeeDetails() {
         return salary;
     }
 
     static class Builder {
-
-        private String name;
-        private String position;
-        private long employeeId;
+        private  String name;
+        private  String position;
+        private  long employeeId;
         private int salary;
 
 
-        public Builder setName(String name) {
+        public Builder setName(String name){
             this.name = name;
             return this;
         }
 
-        public Builder setPosition(String position) {
+        public Builder setPosition(String position){
             this.position = position;
             return this;
         }
 
-        public Builder setEmployeeId(long employeeId) {
+        public Builder setEmployeeId(long employeeId){
             this.employeeId = employeeId;
             return this;
         }
-
-        public Builder setSalary(int salary) {
+        public Builder setSalary(int salary){
             this.salary = salary;
             return this;
         }
 
-        public Manager build() {
-            return new Manager(name, position, employeeId, salary);
+        public Developer build(){
+            return new Developer(name , position , employeeId , salary);
         }
     }
+
+
 }
