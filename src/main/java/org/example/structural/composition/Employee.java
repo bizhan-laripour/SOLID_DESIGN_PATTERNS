5419a210292974eef0e9cbe05db612ac0b5231a7
104913641 <Bizh@n!8830183>
2024-09-24 12:33:41 +0330

    combination of bridge design and abstract factory

---
+++ src/main/java/org/example/structural/composition/Employee.java
@@ -0,0 +1,11 @@
+package org.example.structural.composition;
+
+
+/**
+ * this class acts like component in composition design pattern
+ * this could be a simple leaf or node
+ */
+public interface Employee {
+    int showEmployeeDetails();
+
+}
